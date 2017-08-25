409c6a578dec7f24af08d193e3c07187aa918af0
Luciano Oliveira <luciano101@msn.com>
2017-08-25 13:06:44 -0700

    output total of Pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,36 @@
 //Business Logic
-var Pizza = {
-  basePrice: 12
+
+function Pizza(basePrice, toppingsPrice, size, total) {
+  this.basePrice = 8;
+  this.toppingsPrice = toppingsPrice;
+  this.size = size;
+  this.total = 0;
 }
 
-function Toppings(topping1, topping2, topping3, topping4, topping5, topping6, topping7){
-  this.topping1 = topping1;
-  this.topping2 = topping2;
-  this.topping3 = topping3;
-  this.topping4 = topping4;
-  this.topping5 = topping5;
-  this.topping6 = topping6;
-  this.topping7 = topping7;
+Pizza.prototype.totalPrice = function(){
+  return this.basePrice + this.toppingsPrice + this.size;
 }
 
-var pizzaToppings = new Toppings ('Cheese', 'Pepperoni', 'Bacon', 'Artichoke', 'Tomatoes', 'Spinash', 'Onion');
 
-console.log(pizzaToppings);
+// function Toppings(topping1, topping2, topping3, topping4, topping5, topping6, topping7){
+//   this.topping1 = topping1;
+//   this.topping2 = topping2;
+//   this.topping3 = topping3;
+//   this.topping4 = topping4;
+//   this.topping5 = topping5;
+//   this.topping6 = topping6;
+//   this.topping7 = topping7;
+// }
 
-var Size = {
-  small: "Small",
-  medium: "Medium",
-  large: "Large"
-}
+// var pizzaToppings = new Toppings ('Cheese', 'Pepperoni', 'Bacon', 'Artichoke', 'Tomatoes', 'Spinash', 'Onion');
 
-console.log(Size.large);
+
+
+
+
+
+
+
 
 //Grab values from inputted fiels, such as size and toppings
 //Create prototype method to calculate toppings and size
@@ -31,5 +38,22 @@ console.log(Size.large);
 
 //User Interface Logic
 $(document).ready(function(){
+  var inputToppingsTotal = 0;
+  console.log('here: ', inputToppingsTotal)
   
+  $('form').submit(function(e){
+    e.preventDefault();
+    //grab toppings input
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var $this=$(this).val();
+       inputToppingsTotal = inputToppingsTotal + Number($this);
+       return inputToppingsTotal;
+    });
+    //grap size
+    var inputSize = Number($("select#size option:selected").val());
+    console.log(inputSize);
+    var newPrice  = new Pizza(this.basePice, inputToppingsTotal , inputSize);
+    console.log(newPrice);
+    console.log(newPrice.totalPrice());
+  });
 });
