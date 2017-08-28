2ea64c5ea1dfb6aba462555295a0fe51cb8e1c99
Luciano Oliveira <luciano101@msn.com>
2017-08-27 17:03:57 -0700

    add json array of images, add image folder

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 //Business Logic
 
+var pizzaIngredients  = {
+  toppings: [
+    {
+      name: "Pepperoni",
+      imgId: "pepperoniImg",
+      inputId: "pepperoniInput"
+    },
+    {
+      name: "Cheese",
+      imgId: "pepperoniImg",
+      inputId: "pepperoniInput"
+    },
+    {
+      name: "Tomato",
+      imgId: "tomatoImg",
+      inputId: "tomatoInput"
+    }
+  ]
+}
+
 function Pizza(basePrice, toppingsPrice, size, total) {
   this.basePrice = 8;
   this.toppingsPrice = toppingsPrice;
@@ -12,26 +32,6 @@ Pizza.prototype.totalPrice = function(){
 }
 
 
-// function Toppings(topping1, topping2, topping3, topping4, topping5, topping6, topping7){
-//   this.topping1 = topping1;
-//   this.topping2 = topping2;
-//   this.topping3 = topping3;
-//   this.topping4 = topping4;
-//   this.topping5 = topping5;
-//   this.topping6 = topping6;
-//   this.topping7 = topping7;
-// }
-
-// var pizzaToppings = new Toppings ('Cheese', 'Pepperoni', 'Bacon', 'Artichoke', 'Tomatoes', 'Spinash', 'Onion');
-
-
-
-
-
-
-
-
-
 //Grab values from inputted fiels, such as size and toppings
 //Create prototype method to calculate toppings and size
 //Output total to client
@@ -39,22 +39,23 @@ Pizza.prototype.totalPrice = function(){
 //User Interface Logic
 $(document).ready(function(){
   var inputToppingsTotal = 0;
-  console.log('here: ', inputToppingsTotal)
-  
   $('form').submit(function(e){
     e.preventDefault();
     //grab toppings input
-    $("input:checkbox[name=toppings]:checked").each(function(){
+    $("input:checkbox[name=toppings]:checked").each(function(topping){
       var $this=$(this).val();
        inputToppingsTotal = inputToppingsTotal + Number($this);
        return inputToppingsTotal;
+       console.log(inputToppingsTotal);
     });
-    //grap size
+    //grab size
     var inputSize = Number($("select#size option:selected").val());
-    console.log(inputSize);
     var newPrice  = new Pizza(this.basePrice, inputToppingsTotal , inputSize);
+    //console
+    console.log(inputSize);
     console.log(newPrice);
     console.log(newPrice.totalPrice());
+    //console
     newPrice.total = newPrice.totalPrice()
     console.log('pizzaTotal', newPrice.total);
     $('.output').text(newPrice.total);
